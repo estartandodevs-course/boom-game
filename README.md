f49b2e50880607b775158aeaae9e15eb1a8ba323
teddiur <ryaqueiroz@gmail.com>
2020-11-02 22:16:40 -0300

    score implemented, it was easier than i thought

--- README.md
+++ README.md
@@ -4,9 +4,9 @@ Jogo desenvolvido em aula com alunos Estartando Devs 2020
 # Novas funcionalidades
 - [x] Criação de niveis para o jogo (iniciante, mediano, profissional)
 - [x] Limite de X balões na tela. Caso o número de balões passe: perdeu.
-- [ ] Botão para jogar de novo (trabalhando agr).
+- [x] Botão para jogar de novo (trabalhando agr).
 - [x] Animação no surgimento do balão.
-- [ ] Pontuação (Balões estourados).
+- [x] Pontuação (Balões estourados).
 - [ ] Histórico de maiores pontuações.
 
 Caso tenha sugestões de funcionalidades mande um pull request, adicionando-as nessa lista
--- main.js
+++ main.js
@@ -1,24 +1,36 @@
 const buttonInitGame = document.querySelector(".btn-init-game");
-let level = document.querySelector(".levels input:checked").value
 const buttonsGameLevelContainer = document.querySelector(".levels");
 const buttonsGameLevel = buttonsGameLevelContainer.querySelectorAll("input");
 const balloonsContainer = document.querySelector(".container-balloons");
-const levels = document.querySelector(".game-levels")
-const balloonLimit = 5;
+const scoreNumber = document.querySelector(".score-number");
+const levels = document.querySelector(".game-levels");
+const balloonLimit = 10;
 const timeLevel = {
   easy: 2000,
   medium: 1000,
   hard: 500
-}
+};
+let level = document.querySelector(".levels input:checked").value
+let validPoint = true;
 
 function setLevel() {
   level = this.value
 }
 
+function updateScore() {
+  const score = parseInt(scoreNumber.innerText, 10);
+  scoreNumber.innerText = score + 1;
+}
+function reinitGame() {
+  document.querySelector(".game-over-container").remove();
+  document.querySelectorAll(".balloon").forEach(function (element) { element.remove() });
+  scoreNumber.innerText = 0;
+  validPoint = true;
+}
+
 function initGame() {
   if (document.querySelector(".game-over-container")) {
-    document.querySelector(".game-over-container").remove()
-    document.querySelectorAll(".balloon").forEach(function (element) { element.remove() })
+    reinitGame()
   }
 
   const idInterval = setInterval(manageGame, timeLevel[level]);
@@ -53,7 +65,18 @@ function createBalloon() {
   balloonsContainer.appendChild(elementImg);
 }
 
+function removeBalloon(element) {
+  const boomSound = new Audio("./assets/boom.mpeg");
+  boomSound.play();
+  boomSound.volume = 0.1;
+  if (validPoint) {
+    updateScore()
+  }
+  element.remove();
+}
+
 function showGameOver() {
+  validPoint = false;
   const sectionGameOver = document.createElement("section");
   sectionGameOver.setAttribute("class", "game-over-container");
 
@@ -61,20 +84,17 @@ function showGameOver() {
   textGameOver.innerText = "Você perdeu, loser!";
   textGameOver.style.color = "#000000";
 
+  const finalScore = document.createElement("p")
+  finalScore.innerText = `Pontuação final: ${scoreNumber.innerText}`
+
   buttonInitGame.style.display = "initial";
   buttonInitGame.style.bottom = "5px"
   buttonInitGame.innerText = "Jogar novamente"
 
   sectionGameOver.appendChild(textGameOver);
+  sectionGameOver.appendChild(finalScore);
   sectionGameOver.appendChild(buttonInitGame);
   balloonsContainer.appendChild(sectionGameOver);
-}
-
-function removeBalloon(element) {
-  const boomSound = new Audio("./assets/boom.mpeg");
-  boomSound.play();
-  boomSound.volume = 0.1;
-  element.remove();
 }
 
 buttonsGameLevel.forEach((button) => { button.addEventListener("click", setLevel) });
